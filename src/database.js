07f68621255c65f043d9f2f8cd5944518c39de9f
V11Playko <heinnervega20@gmail.com>
2022-05-02 08:30:37 -0500

    Haciendo moongose models

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 (async () => {
   try {
     const db = await connect(
-      "mongodb+srv://playko:komodor11@apidb.lldrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+      "mongodb+srv://playko:komodor11@apidb.lldrd.mongodb.net/apiDB?retryWrites=true&w=majority"
     );
     console.log("Db connectect to", db.connection.name);
   } catch (error) {
