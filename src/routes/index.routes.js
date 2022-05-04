b47bd1a9b63b6e0030db11f5c13eeaf70d8519cf
V11Playko <heinnervega20@gmail.com>
2022-05-04 10:47:27 -0500

    Cambiar estado de tareas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -39,10 +39,22 @@ router.post("/edit/:id", async (req, res) => {
   res.redirect("/");
 });
 
-router.get("delete/:{id}", async (req, res) => {
+router.get("delete/:id", async (req, res) => {
   const { id } = req.params;
 
   await Task.findByIdAndDelete(id);
+});
+
+router.get("/toggleDone/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findById(id);
+
+  task.done = !task.done;
+
+  await task.save();
+
+  res.redirect("/");
 });
 
 export default router;
